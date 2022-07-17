47c9a0d7e4fb368d0d9c05e61782cbc550f92f1d
Amin Saedi <amin.di.sam@gmail.com>
2022-07-17 10:50:24 -0400

    Clear command fixed

--- reminders/clear_reminders.py
+++ reminders/clear_reminders.py
@@ -1,19 +1,15 @@
 import yaml
 import pathlib
 
+
 def clear():
-	current_path = str(pathlib.Path(__file__).parent.absolute())
-
-	with open(current_path + '/data.yml', 'r') as file:
-		data = yaml.safe_load(file)
-
-	for x in range(7):
-		data[x] = []
-    #for i in range(7):
-    #    data[i] = []
+    current_path = str(pathlib.Path(__file__).parent.absolute())
+    with open(current_path + '/data.yml', 'r') as file:
+        data = yaml.safe_load(file)
+        print(data)
+    for i in range(7):
+        data[str(i)] = []
+    print(data)
     with open(current_path + '/data.yml', 'w') as file:
-		try:
-			file.truncate(0)
-			yaml.dump(data, file)
-		except:
-			print("Error on writing to file")
+        yaml.dump(data, file)
+
